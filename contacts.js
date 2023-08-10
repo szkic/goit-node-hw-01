4cbdd95bf5cc3f8069de757585dff73e505002f8
bartosz.sz <bartosz.szkiladz@gmail.com>
2023-08-10 17:41:59 +0200

    app

---
+++ contacts.js
@@ -0,0 +1,54 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { nanoid } from "nanoid";
+
+const contactsPath = path.format({
+  dir: "db",
+  base: "contacts.json",
+});
+
+export function listContacts() {
+  fs.readFile(contactsPath)
+    .then((data) => console.log(JSON.parse(data)))
+    .catch((error) => console.log(error));
+}
+
+export function getContactById(contactId) {
+  fs.readFile(contactsPath)
+    .then((data) => {
+      const parsedData = JSON.parse(data);
+      const getContact = parsedData.find((option) => option.id === contactId);
+
+      console.log(getContact);
+    })
+    .catch((error) => console.log(error));
+}
+
+export function removeContact(contactId) {
+  fs.readFile(contactsPath)
+    .then((data) => {
+      const parsedData = JSON.parse(data);
+      const contacts = parsedData.filter((option) => option.id !== contactId);
+
+      fs.writeFile(contactsPath, JSON.stringify(contacts));
+    })
+    .catch((error) => console.log(error));
+}
+
+export function addContact(name, email, phone) {
+  const contact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+
+  fs.readFile(contactsPath)
+    .then((data) => {
+      const parsedData = JSON.parse(data);
+      parsedData.push(contact);
+
+      fs.writeFile(contactsPath, JSON.stringify(parsedData));
+    })
+    .catch((error) => console.log(error));
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+https://ibb.co/g6f1kdM
+https://ibb.co/stghxNP
+https://ibb.co/60rWRN1
+https://ibb.co/jH93JNT
+https://ibb.co/b1jN8XS
